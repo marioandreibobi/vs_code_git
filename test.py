3c373503981de4cc4abdcbc6428b4dd48f6a97cb
marioandreibobi <bobimarioandrei@gmail.com>
2026-02-24 14:24:26 +0200

    Hei asta este unt test

--- test.py
+++ test.py
@@ -1,38 +1,31 @@
-
+#exercitiu 1
+"""
 matrix = [
     [1, 2, 3],
-    [4, 5, 6], 
+    [5,2,6], 
     [7, 8, 9]
 ]
 
-crestere = True
+count=0
 
-nume = {"ANA", "ION", "MARIA"}
+for row in matrix:
+    increasing = True
+    for i in range(len(row)-1):
+        if row[i]>row[i+1]:
+            increasing=False
+            break
+    if increasing:
+        count+=1
 
-# printăm numele
-for i in nume:
-    print(i)
 
-# definim funcția Fibonacci la nivel global
-def fib(n):
-    """
-    Construiește și scrie șirul lui Fibonacci până la n și întoarce lista ca rezultat
-    I: n - limita superioară
-    E: lista termenilor calculați
-    """
-    result = []
-    a, b = 0, 1
-    while a < n:
-        print(a, end=' ')
-        result.append(a)
-        a, b = b, a + b
-    print("<== gata")
-    return result
+print("Numar de randuri crescatoare: ", count)
 
-print('Al doilea text')
+"""
+#exercitiu 2
 
-# rulăm funcția doar dacă scriptul e executat direct
-if __name__ == '__main__':
-    a = fib(50)
-    print('Lista generata:')
-    print(a)
+matrix = [
+    [1, 2, 3],
+    [5,2,6], 
+    [7, 8, 9]
+]
+
